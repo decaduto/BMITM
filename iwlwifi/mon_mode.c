db7f69cc6842b0ee0974f5126521a7ae87cebdbc
decaduto <78024779+decaduto@users.noreply.github.com>
2022-03-04 08:37:01 +0100

    //

--- iwlwifi/mon_mode.c
+++ iwlwifi/mon_mode.c
@@ -23,3 +23,19 @@ int iwl_dnt_dev_if_start_monitor(struct iwl_dnt *dnt,
 		return -EINVAL;
 	}
 }
+
+static int iwl_dnt_dev_if_send_ldbg(struct iwl_dnt *dnt,
+				    struct iwl_trans *trans,
+				    int cmd_index)
+{
+	struct iwl_dbg_cfg *cfg = &trans->dbg_cfg;
+	struct iwl_host_cmd host_cmd = {
+		.id = cfg->dbg_conf_monitor_cmd_id,
+		.data[0] = cfg->ldbg_cmd[cmd_index].data,
+		.len[0] = DNT_LDBG_CMD_SIZE,
+		.dataflags[0] = IWL_HCMD_DFL_NOCOPY,
+	};
+
+	return iwl_trans_send_cmd(trans, &host_cmd);
+}
+
